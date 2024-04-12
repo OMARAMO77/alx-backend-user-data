ddf20d3a8c292614973496c0df90bd5006138129
Omar <omar0642624692@gmail.com>
2024-04-11 18:31:54 -0700

    encrypt_password.py

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -11,3 +11,11 @@ def hash_password(password: str) -> bytes:
     salted = bcrypt.gensalt()
     hashed = bcrypt.hashpw(encoded, salted)
     return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Checks is a hashed password was formed from the given password.
+    """
+    encoded = password.encode('utf-8')
+    result = bcrypt.checkpw(encoded, hashed_password)
+    return result
