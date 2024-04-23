41ede48b95163ced8ca7fbb6e5ff69304d7170a5
Omar <omar0642624692@gmail.com>
2024-04-23 13:24:56 -0700

    app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -53,5 +53,16 @@ def logout():
     return redirect('/')
 
 
+@app.route("/profile", strict_slashes=False)
+def profile():
+    """Gets and returns json data of user profile
+    """
+    session_id = request.cookies.get("session_id", None)
+    user = AUTH.get_user_from_session_id(session_id)
+    if user is None:
+        abort(403)
+    return jsonify({"email": user.email})
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
