a1155544cc22f4890bf26537336047fb8eb0ac25
Omar <omar0642624692@gmail.com>
2024-04-23 14:06:47 -0700

    app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -75,6 +75,5 @@ def get_reset_password_token():
         abort(403)
     return jsonify({"email": email, "reset_token": reset_token})
 
-
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
